aa26672ac84e6d307b9e6a18380bb60176ad8d56
quangtt-pc06371 <151135993+quangtt-pc06371@users.noreply.github.com>
2024-08-05 15:08:42 +0700

    update loginfe2

--- src/services/Auth/index.js
+++ src/services/Auth/index.js
@@ -4,11 +4,10 @@ const loginApi = async ({ id, password }) => {
   try {
     const res = await request({
       method: "POST",
-      path: "api//auth/login",
+      path: "api/auth/login",
       data: {
         id,
         password,
-        // device: "website" // Uncomment if you need to include device information
       },
     });
 
@@ -27,7 +26,7 @@ const loginApi = async ({ id, password }) => {
     };
   } catch (error) {
     console.error("Lỗi khi gọi API đăng nhập:", error);
-    throw error;
+    throw new Error(error);
   }
 };
 
@@ -53,4 +52,3 @@ const getProfile = async () => {
 };
 
 export { loginApi, getProfile };
-
